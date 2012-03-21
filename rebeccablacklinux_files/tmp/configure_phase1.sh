7aed4123d1046f10b1b715b5e2378c216b096a6b
nerdopolis <nerdopolis@b52b6941-3400-464c-9f42-43200397181c>
2012-03-21 03:55:57 +0000

    remove extra file in /tmp
    Try to fix the possiblity that a package might no longer need to be installed after a new version. Can't specify REMOVE. Try this. Still in testing. 
    
    I deleted my compile_all script by mistake!
    
    git-svn-id: https://svn.code.sf.net/p/rebeccablackos/code@176 b52b6941-3400-464c-9f42-43200397181c

--- rebeccablacklinux_files/tmp/configure_phase1.sh
+++ rebeccablacklinux_files/tmp/configure_phase1.sh
@@ -35,58 +35,116 @@ export DEBIAN_FRONTEND=noninteractive
 #update the apt cache
 apt-get update
 
-#install aptitude
-echo Y | apt-get install aptitude
+#LIST OF PACKAGES TO GET INSTALLED
+BINARYINSTALLS="aptitude
+language-pack-en 
+linux-generic
+llvm
+libxkbcommon-dev
+build-essential 
+libtool 
+libxi-dev 
+libxmu-dev 
+libxt-dev 
+bison flex 
+libgl1-mesa-dev 
+xutils-dev 
+libtalloc-dev 
+libdrm-dev 
+autoconf 
+x11proto-kb-dev 
+libegl1-mesa-dev 
+libgles2-mesa-dev 
+libgdk-pixbuf2.0-dev 
+libudev-dev 
+libxcb-dri2-0-dev 
+libxcb-xfixes0-dev 
+shtool 
+libffi-dev 
+libpoppler-glib-dev 
+libgtk2.0-dev 
+git diffstat 
+libx11-xcb-dev 
+quilt 
+autopoint 
+dh-autoreconf 
+xkb-data 
+gtk-doc-tools 
+gobject-introspection 
+gperf 
+librsvg2-bin 
+libpciaccess-dev  
+python-libxml2 
+libjpeg-dev   
+libgbm-dev 
+libxcb-glx0-dev 
+libgl1-mesa-dri-dbg
+kubuntu-desktop 
+ubuntu-standard 
+firefox 
+epiphany-browser 
+evolution 
+gedit 
+gnibbles 
+unity 
+nautilus 
+file-roller 
+cheese
+clutter-1.0-tests
+libxcb1 
+libxcb1-dev 
+libx11-xcb1 
+libx11-xcb-dev 
+libxcb-keysyms1 
+libxcb-keysyms1-dev 
+libxcb-image0 
+libxcb-image0-dev 
+libxcb-shm0 
+libxcb-shm0-dev 
+libxcb-icccm4 
+libxcb-icccm4-dev 
+libxcb-sync0 
+libxcb-sync0-dev 
+libxcb-xfixes0-dev 
+libgtk-3-dev 
+libgio2.0-cil-dev 
+libjson-glib-dev
+x11proto-xcmisc-dev   
+x11proto-bigreqs-dev 
+x11proto-fonts-dev  
+x11proto-video-dev 
+x11proto-record-dev 
+x11proto-resource-dev 
+libxkbfile-dev 
+libxfont-dev 
+xserver-xorg-dev 
+x11proto-xf86dri-dev 
+subversion 
+e17
+libraptor-dev
+remastersys"
 
-#Add Language packs as translations are added
-aptitude install language-pack-en --without-recommends -y
+#LIST OF PACKAGES THAT NEED BUILD DEPS
+BUILDINSTALLS="libgtk-3-0 
+libgtk2.0-0
+libclutter-1.0-0
+mesa
+tinc
+e17  
+kde4libs"
 
+#INSTALL THE PACKAGES SPECIFIED
+echo "$BINARYINSTALLS" | while read PACKAGE
+do
+echo Y | apt-get install $PACKAGE -y
+done
 
-#install a kernel for the Live disk
-aptitude install linux-generic  --without-recommends -y
+#GET BUILDDEPS FOR THE PACKAGES SPECIFIED
+echo "$BUILDINSTALLS" | while read PACKAGE
+do
+echo Y | apt-get build-dep $PACKAGE -y
+done
 
-#install more mesa depends
-yes Y | apt-get build-dep mesa
-yes Yes | apt-get install llvm -y
-
-#Install Wayland depends 
-yes Yes | apt-get install libxkbcommon-dev build-essential libtool libxi-dev libxmu-dev libxt-dev bison flex libgl1-mesa-dev xutils-dev libtalloc-dev libdrm-dev autoconf x11proto-kb-dev libegl1-mesa-dev libgles2-mesa-dev libgdk-pixbuf2.0-dev libudev-dev libxcb-dri2-0-dev libxcb-xfixes0-dev shtool libffi-dev libpoppler-glib-dev libgtk2.0-dev git diffstat libx11-xcb-dev quilt autopoint dh-autoreconf xkb-data gtk-doc-tools gobject-introspection gperf librsvg2-bin libpciaccess-dev  python-libxml2 libjpeg-dev   libgbm-dev libxcb-glx0-dev libgl1-mesa-dri-dbg -y
-
-#install Desktops
-yes Yes |apt-get install kubuntu-desktop ubuntu-standard firefox -y
-yes Yes |apt-get install epiphany-browser evolution gedit gnibbles unity nautilus file-roller cheese -y
-
-
-#install for testing clutter
-yes Yes |apt-get install  clutter-1.0-tests -y
-
-#install depends for building QT
-yes Yes | aptitude install libxcb1 libxcb1-dev libx11-xcb1 libx11-xcb-dev libxcb-keysyms1 libxcb-keysyms1-dev libxcb-image0 libxcb-image0-dev libxcb-shm0 libxcb-shm0-dev libxcb-icccm4 libxcb-icccm4-dev libxcb-sync0 libxcb-sync0-dev libxcb-xfixes0-dev -y
-
-#install depends for building gtk
-yes Y | apt-get build-dep libgtk-3-0 libgtk2.0-0 
-yes Yes | aptitude install libgtk-3-dev libgio2.0-cil-dev  -y
-
-#install depends for building clutter
-yes Yes | aptitude install libjson-glib-dev
-
-#Install depends for building xwayland (nested X under Wayland)
-yes Yes | aptitude install x11proto-xcmisc-dev   x11proto-bigreqs-dev x11proto-fonts-dev  x11proto-video-dev x11proto-record-dev x11proto-resource-dev libxkbfile-dev libxfont-dev xserver-xorg-dev x11proto-xf86dri-dev -y
-yes Y | apt-get build-dep tinc
-
-#install clutter depends
-yes Y | apt-get build-dep libclutter-1.0-0
-
-#install efl depends
-yes Y | apt-get install subversion e17
-yes Y | apt-get build-dep e17  
-
-#get depends for kdelibs
-yes Y | apt-get build-dep kde4libs
-yes Y | apt-get install libraptor-dev
-
-#download remastersys
-#yes Yes | aptitude install remastersys -y --download-only
 ##################################################################################################################
 
 
@@ -101,6 +159,36 @@ ln -s /bin/true /sbin/initctl
 yes Y | apt-get dist-upgrade
 rm /sbin/initctl
 mv  /sbin/initctl.bak /sbin/initctl
+
+#get current list of packages
+dpkg --get-selections > /tmp/oldpackages
+
+#backup the state of the packages
+cp /var/lib/dpkg/status  /var/lib/dpkg/status.backup
+
+#trick dpkg to think everytiing is uninstalled
+sed -i 's/install ok installed/deinstall ok config-files' /var/lib/dpkg/status
+
+#FAKE INSTALL THE PACKAGES SPECIFIED TO GET LIST OF DEPENDS
+echo "$BINARYINSTALLS" | while read PACKAGE
+do
+echo Y | apt-get install $PACKAGE -y -s >> /tmp/newpackages
+done
+
+#FAKE BUILDDEPS FOR THE PACKAGES SPECIFIED TO GET LIST OF DEPENDS
+echo "$BUILDINSTALLS" | while read PACKAGE
+do
+echo Y | apt-get build-dep $PACKAGE -y -s >> /tmp/newpackages
+done
+
+#Place the state of the packages back
+cp /var/lib/dpkg/status.backup  /var/lib/dpkg/status
+
+#find packages that WHERE installed previously, but are no longer specified to BE installed
+diff /tmp/oldpackages /tmp/newpackages | grep \< | awk '{print $2}' | while read PACKAGE
+do
+apt-get purge $PACKAGE
+done
 
 #run the script that calls all compile scripts in a specified order, in download only mode
 compile_all download-only
