16cecc522ed1c1da6a7e3088a9d2673410571531
nerdopolis <nerdopolis@b52b6941-3400-464c-9f42-43200397181c>
2012-03-24 03:15:55 +0000

    Import the build scripts as well
    git-svn-id: https://svn.code.sf.net/p/rebeccablackos/code@193 b52b6941-3400-464c-9f42-43200397181c

--- externalbuilders/rebeccablacklinux_phase1.sh
+++ externalbuilders/rebeccablacklinux_phase1.sh
@@ -68,6 +68,7 @@ chgrp  root  -R ~/RBOS_Build_Files/build_mountpoint/phase_1/temp/
 
 #copy the ONLY minimal build files in, not any data files like wallpapers.
 rsync ~/RBOS_Build_Files/build_mountpoint/phase_1/temp/tmp/* -a ~/RBOS_Build_Files/build_mountpoint/phase_1/tmp
+cp -a ~/RBOS_Build_Files/build_mountpoint/phase_1/temp/usr/bin/Compile/ ~/RBOS_Build_Files/build_mountpoint/phase_1/usr/bin/Compile/
 cp ~/RBOS_Build_Files/build_mountpoint/phase_1/temp/usr/bin/compile_all ~/RBOS_Build_Files/build_mountpoint/phase_1/usr/bin/compile_all 
 cp ~/RBOS_Build_Files/build_mountpoint/phase_1/temp/etc/apt/sources.list ~/RBOS_Build_Files/build_mountpoint/phase_1/etc/apt/sources.list 
 
