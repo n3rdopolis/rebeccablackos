21e85a21aec07a33428298ecf709eebaafb61572
nerdopolis <nerdopolis@b52b6941-3400-464c-9f42-43200397181c>
2013-01-24 23:59:01 +0000

    Fix up phase 3 helper for the building of  the deb
    git-svn-id: https://svn.code.sf.net/p/rebeccablackos/code@1010 b52b6941-3400-464c-9f42-43200397181c

--- rebeccablacklinux_files/tmp/configure_phase3_helper.sh
+++ rebeccablacklinux_files/tmp/configure_phase3_helper.sh
@@ -17,13 +17,28 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #Copy select files into place, that are suitable for distribution.
+mkdir -p /usr/bin
 rsync /usr/import/usr/bin/* -a /usr/bin
+
+mkdir -p /usr/libexec
 rsync /usr/import/usr/libexec/* -a /usr/libexec
+
+mkdir -p /usr/share/RBOS_MENU
 rsync /usr/import/usr/share/RBOS_MENU/* -a /usr/share/RBOS_MENU
+
+mkdir -p /usr/share/RBOS_PATCHES
 rsync /usr/import/usr/share/RBOS_PATCHES/* -a /usr/share/RBOS_PATCHES
+
+mkdir -p /usr/share/icons
 rsync /usr/import/usr/share/icons/* -a /usr/share/icons
+
+mkdir -p /usr/share/wallpapers/RebeccaBlackOS/
 rsync /usr/import/usr/share/wallpapers/RebeccaBlackOS/* -a /usr/share/wallpapers/RebeccaBlackOS/
-rsync /usr/import/etc/skel/* -a /etc/skel
+
+mkdir -p /etc/skel/.config
+rsync /usr/import/etc/skel/.config/* -a /etc/skel/.config
+
+mkdir -p /var
 rsync /usr/import/var/* -a /var
 #install the menu items for the wayland tests
 install_menu_items
