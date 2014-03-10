b2a080a24143901a465c2309a8eb576219b91999
nerdopolis <nerdopolis@b52b6941-3400-464c-9f42-43200397181c>
2014-03-10 00:56:28 +0000

    Under request, switch to a newer Kernel from a PPA, and create a reliable way of downloading and installing it so that it only runs once
    git-svn-id: https://svn.code.sf.net/p/rebeccablackos/code@2276 b52b6941-3400-464c-9f42-43200397181c

--- rebeccablacklinux_files/tmp/configure_phase2.sh
+++ rebeccablacklinux_files/tmp/configure_phase2.sh
@@ -130,6 +130,31 @@ done < <(echo "$INSTALLS")
 
 cp /tmp/INSTALLS.txt /tmp/INSTALLS.txt.installbak
 
+#Install a new kernel not in the Ubuntu archives
+cd /var/cache/apt/archives/
+
+PKGNAME=$(dpkg-deb -W linux-headers-3.13.6-*all.deb | awk '{print $1}')
+PKGINSTALLCOUNT=$(dpkg --get-selections | awk '{print $1}' | grep -c $PKGNAME$ )
+if [[ $PKGINSTALLCOUNT == 0 ]]
+then
+  dpkg -i linux-headers-3.13.6-*all.deb
+fi
+
+PKGNAME=$(dpkg-deb -W linux-headers-3.13.6-*$BUILDARCH.deb | awk '{print $1}')
+PKGINSTALLCOUNT=$(dpkg --get-selections | awk '{print $1}' | grep -c $PKGNAME$ )
+if [[ $PKGINSTALLCOUNT == 0 ]]
+then
+  dpkg -i linux-headers-3.13.6-*$BUILDARCH.deb
+fi
+
+PKGNAME=$(dpkg-deb -W linux-image-3.13.6-*$BUILDARCH.deb | awk '{print $1}')
+PKGINSTALLCOUNT=$(dpkg --get-selections | awk '{print $1}' | grep -c $PKGNAME$ )
+if [[ $PKGINSTALLCOUNT == 0 ]]
+then
+  dpkg -i linux-image-3.13.6-*$BUILDARCH.deb
+fi
+
+
 
 #remove old kernels!
 CURRENTKERNELVERSION=$(basename $(readlink /vmlinuz) |awk -F "-" '{print $2"-"$3}')
