c86772b09f3d6e9e48aedff5d6e2f9ed4673ec80
nerdopolis <nerdopolis@b52b6941-3400-464c-9f42-43200397181c>
2014-04-10 02:46:00 +0000

    Fix the journal persistance. by making the folder.
    git-svn-id: https://svn.code.sf.net/p/rebeccablackos/code@2398 b52b6941-3400-464c-9f42-43200397181c

--- rebeccablacklinux_files/tmp/configure_phase3.sh
+++ rebeccablacklinux_files/tmp/configure_phase3.sh
@@ -64,6 +64,7 @@ systemctl enable resolvconf.service
 systemctl enable unset-grub-fail.service
 
 #Configure users that can see journal logs
+mkdir -p /var/log/journal
 sed -i 's/systemd-journal/sudo/g' /usr/lib/tmpfiles.d/systemd.conf
 
 #Change the default init system to systemd if it exists
