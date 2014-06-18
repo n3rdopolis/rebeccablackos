ec04b55d37fdd1025c80348807cdaf0012f7a440
nerdopolis <nerdopolis@b52b6941-3400-464c-9f42-43200397181c>
2014-06-18 23:34:53 +0000

    Configure_phase3: make sure removes.txt gets logged in the correct place.
    git-svn-id: https://svn.code.sf.net/p/rebeccablackos/code@2606 b52b6941-3400-464c-9f42-43200397181c

--- rebeccablacklinux_files/tmp/configure_phase3.sh
+++ rebeccablacklinux_files/tmp/configure_phase3.sh
@@ -95,24 +95,24 @@ ln -s /bin/true /usr/sbin/invoke-rc.d
 #This will remove my abilities to build packages from the ISO, but should make it a bit smaller
 REMOVEDEVPGKS=$(dpkg --get-selections | awk '{print $1}' | grep "\-dev$"  | grep -v python-dbus-dev | grep -v dpkg-dev)
 
-apt-get purge $REMOVEDEVPGKS -y --force-yes | tee /usr/share/logs/package_operations/removes.txt
+apt-get purge $REMOVEDEVPGKS -y --force-yes | tee /tmp/logs/package_operations/removes.txt
 
 
 REMOVEDEVPGKS=$(dpkg --get-selections | awk '{print $1}' | grep "\-dev:"  | grep -v python-dbus-dev | grep -v dpkg-dev)
-apt-get purge $REMOVEDEVPGKS -y --force-yes | tee -a /usr/share/logs/package_operations/removes.txt
+apt-get purge $REMOVEDEVPGKS -y --force-yes | tee -a /tmp/logs/package_operations/removes.txt
 
 
 REMOVEDEVPGKS=$(dpkg --get-selections | awk '{print $1}' | grep "\-dbg$"  | grep -v python-dbus-dev | grep -v dpkg-dev)
-apt-get purge $REMOVEDEVPGKS -y --force-yes | tee -a /usr/share/logs/package_operations/removes.txt
+apt-get purge $REMOVEDEVPGKS -y --force-yes | tee -a /tmp/logs/package_operations/removes.txt
 
 REMOVEDEVPGKS=$(dpkg --get-selections | awk '{print $1}' | grep "\-dbg:"  | grep -v python-dbus-dev | grep -v dpkg-dev)
-apt-get purge $REMOVEDEVPGKS -y --force-yes | tee -a /usr/share/logs/package_operations/removes.txt
+apt-get purge $REMOVEDEVPGKS -y --force-yes | tee -a /tmp/logs/package_operations/removes.txt
 
 REMOVEDEVPGKS="texlive-base ubuntu-docs gnome-user-guide cmake libgl1-mesa-dri-dbg libglib2.0-doc valgrind cmake-rbos smbclient freepats libc6-dbg doxygen git subversion bzr mercurial checkinstall texinfo"
-apt-get purge $REMOVEDEVPGKS -y --force-yes | tee -a /usr/share/logs/package_operations/removes.txt
+apt-get purge $REMOVEDEVPGKS -y --force-yes | tee -a /tmp/logs/package_operations/removes.txt
 
 
-apt-get autoremove -y --force-yes >> /usr/share/logs/package_operations/removes.txt
+apt-get autoremove -y --force-yes >> /tmp/logs/package_operations/removes.txt
 
 #Reset the utilites back to the way they are supposed to be.
 rm /sbin/initctl
