3917dc05ca9acee94f3e0af12f39147216df288c
nerdopolis <nerdopolis@b52b6941-3400-464c-9f42-43200397181c>
2014-10-11 16:23:52 +0000

    Ubuntu upstream packages now have systemd files, so I no longer need them in the SVN
    git-svn-id: https://svn.code.sf.net/p/rebeccablackos/code@2902 b52b6941-3400-464c-9f42-43200397181c

--- rebeccablacklinux_files/tmp/configure_phase3_helper.sh
+++ rebeccablacklinux_files/tmp/configure_phase3_helper.sh
@@ -58,9 +58,6 @@ cp -a /usr/import/etc/sysctl.d/* /etc/sysctl.d
 mkdir -p /lib
 cp -a /usr/import/lib/* /lib
 
-mkdir -p /etc/systemd
-cp -a /usr/import/etc/systemd/* /etc/systemd
-
 mkdir -p /etc/init
 cp -a /usr/import/etc/init/* /etc/init
 
