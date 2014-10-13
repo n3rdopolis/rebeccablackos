11a90b47d98741425cb157bae074554b8b545e2d
nerdopolis <nerdopolis@b52b6941-3400-464c-9f42-43200397181c>
2014-10-13 21:06:45 +0000

    Don't import the BUILDARCH in the file name
    git-svn-id: https://svn.code.sf.net/p/rebeccablackos/code@2927 b52b6941-3400-464c-9f42-43200397181c

--- externalbuilders/rebeccablacklinux_phase1.sh
+++ externalbuilders/rebeccablacklinux_phase1.sh
@@ -43,11 +43,11 @@ rm -rf "$BUILDLOCATION"/build/$BUILDARCH/importdata/
 rsync "$SCRIPTFOLDERPATH"/../rebeccablacklinux_files/* -Cr "$BUILDLOCATION"/build/$BUILDARCH/importdata/
 
 #Support importing the control file to use fixed revisions of the source code
-rm "$BUILDLOCATION"/build/$BUILDARCH/importdata/tmp/RebeccaBlackLinux_Revisions_$BUILDARCH.txt
-rm "$BUILDLOCATION"/build/$BUILDARCH/workdir/tmp/RebeccaBlackLinux_Revisions_$BUILDARCH.txt
-if [[ -e "$BUILDLOCATION"/RebeccaBlackLinux_Revisions_$BUILDARCH.txt ]]
+rm "$BUILDLOCATION"/build/$BUILDARCH/importdata/tmp/RebeccaBlackLinux_Revisions.txt
+rm "$BUILDLOCATION"/build/$BUILDARCH/workdir/tmp/RebeccaBlackLinux_Revisions.txt
+if [[ -e "$BUILDLOCATION"/RebeccaBlackLinux_Revisions.txt ]]
 then
-  cp "$BUILDLOCATION"/RebeccaBlackLinux_Revisions_$BUILDARCH.txt "$BUILDLOCATION"/build/$BUILDARCH/importdata/tmp/
+  cp "$BUILDLOCATION"/RebeccaBlackLinux_Revisions_$BUILDARCH.txt "$BUILDLOCATION"/build/$BUILDARCH/importdata/tmp/RebeccaBlackLinux_Revisions.txt
   rm "$BUILDLOCATION"/RebeccaBlackLinux_Revisions_$BUILDARCH.txt
 fi
 
