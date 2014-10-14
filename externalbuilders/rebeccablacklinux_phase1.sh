eacc302c611154f0975a7c667c8d6e9868c0784b
nerdopolis <nerdopolis@b52b6941-3400-464c-9f42-43200397181c>
2014-10-14 02:40:11 +0000

    Fix import of the source revision control file
    git-svn-id: https://svn.code.sf.net/p/rebeccablackos/code@2939 b52b6941-3400-464c-9f42-43200397181c

--- externalbuilders/rebeccablacklinux_phase1.sh
+++ externalbuilders/rebeccablacklinux_phase1.sh
@@ -45,7 +45,7 @@ rsync "$SCRIPTFOLDERPATH"/../rebeccablacklinux_files/* -Cr "$BUILDLOCATION"/buil
 #Support importing the control file to use fixed revisions of the source code
 rm "$BUILDLOCATION"/build/$BUILDARCH/importdata/tmp/buildcore_revisions.txt
 rm "$BUILDLOCATION"/build/$BUILDARCH/workdir/tmp/buildcore_revisions.txt
-if [[ -e "$BUILDLOCATION"/RebeccaBlackLinux_Revisions.txt ]]
+if [[ -e "$BUILDLOCATION"/RebeccaBlackLinux_Revisions_$BUILDARCH.txt ]]
 then
   cp "$BUILDLOCATION"/RebeccaBlackLinux_Revisions_$BUILDARCH.txt "$BUILDLOCATION"/build/$BUILDARCH/importdata/tmp/buildcore_revisions.txt
   rm "$BUILDLOCATION"/RebeccaBlackLinux_Revisions_$BUILDARCH.txt
