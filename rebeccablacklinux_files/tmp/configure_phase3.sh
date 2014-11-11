301fa198ee96b3793d0b1930a24a4d13a4124975
nerdopolis <nerdopolis@b52b6941-3400-464c-9f42-43200397181c>
2014-11-11 13:44:59 +0000

    No more need for the configure-soundpermissions hack. It was because I was missing ACL support for systemd
    git-svn-id: https://svn.code.sf.net/p/rebeccablackos/code@3080 b52b6941-3400-464c-9f42-43200397181c

--- rebeccablacklinux_files/tmp/configure_phase3.sh
+++ rebeccablacklinux_files/tmp/configure_phase3.sh
@@ -63,7 +63,6 @@ systemctl enable make-machine-id.service
 systemctl enable mount-run-shm.service
 systemctl enable configure-resolvconf.service
 systemctl enable unset-grub-fail.service
-systemctl enable configure-soundpermissions.service
 systemctl enable waylandloginmanager.service
 systemctl disable friendly-recovery.service
 systemctl disable lightdm.service
