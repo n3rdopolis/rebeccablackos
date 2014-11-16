ba7d92eae86f65893b1043c124d1e9f4a8c29353
nerdopolis <nerdopolis@b52b6941-3400-464c-9f42-43200397181c>
2014-11-16 23:07:36 +0000

    Fix the issue with icons in gnome-control-center correctly
    git-svn-id: https://svn.code.sf.net/p/rebeccablackos/code@3116 b52b6941-3400-464c-9f42-43200397181c

--- rebeccablacklinux_files/tmp/configure_phase3.sh
+++ rebeccablacklinux_files/tmp/configure_phase3.sh
@@ -94,8 +94,6 @@ ln -s /usr/bin/westonlaunchcaller /opt/bin/weston
 glib-compile-schemas /opt/share/glib-2.0/schemas 
 
 #Configure gnome-shell to use the unity control panel
-dpkg-divert --local --rename --add /usr/bin/gnome-control-center
-ln -s /usr/bin/unity-control-center /usr/bin/gnome-control-center
 sed 's/Exec=/Exec=waylandapp /g' /usr/share/applications/gnome-control-center.desktop > /opt/share/applications/gnome-control-center.desktop
 sed 's/Exec=/Exec=waylandapp /g' /usr/share/applications/gnome-background-panel.desktop > /opt/share/applications/gnome-background-panel.desktop
 
