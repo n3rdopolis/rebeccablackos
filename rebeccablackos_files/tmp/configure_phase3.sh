6d3787d011dd92622df4cc28125a20d5e8eefe5f
nerdopolis <nerdopolis@b52b6941-3400-464c-9f42-43200397181c>
2015-05-31 22:34:44 +0000

    Workaround to get Ubiquity to work
    git-svn-id: https://svn.code.sf.net/p/rebeccablackos/code@3609 b52b6941-3400-464c-9f42-43200397181c

--- rebeccablackos_files/tmp/configure_phase3.sh
+++ rebeccablackos_files/tmp/configure_phase3.sh
@@ -110,6 +110,11 @@ glib-compile-schemas /opt/share/glib-2.0/schemas
 sed 's/Exec=/Exec=waylandapp /g' /usr/share/applications/gnome-control-center.desktop > /opt/share/applications/gnome-control-center.desktop
 sed 's/Exec=/Exec=waylandapp /g' /usr/share/applications/gnome-background-panel.desktop > /opt/share/applications/gnome-background-panel.desktop
 
+#ubiquity workaround. GTK in /usr tries to connect to a display server that isn't Wayland, or X11
+dpkg-divert --add --rename --divert /usr/bin/ubiquity.real /usr/bin/ubiquity
+echo -e "#! /bin/bash\nwlsudo ubiquity" > /usr/bin/ubiquity
+chmod +x /usr/bin/ubiquity
+
 #copy all files again to ensure that the SVN versions are not overwritten by a checkinstalled version
 rsync /usr/import/* -a /
 
