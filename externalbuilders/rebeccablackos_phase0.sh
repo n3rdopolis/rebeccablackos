465eead66a1389f114791ede21d1816ee20210d6
nerdopolis <nerdopolis@b52b6941-3400-464c-9f42-43200397181c>
2015-06-23 12:25:51 +0000

    Unhardcode using one Ubuntu mirror, use the main Ubuntu link. The other one was faster in 2012, but now that doesn't affect the build speed for each ISO, because they aren't starting from scratch each time.
    git-svn-id: https://svn.code.sf.net/p/rebeccablackos/code@3646 b52b6941-3400-464c-9f42-43200397181c

--- externalbuilders/rebeccablackos_phase0.sh
+++ externalbuilders/rebeccablackos_phase0.sh
@@ -57,7 +57,7 @@ export DEBOOTSTRAP_DIR="$BUILDLOCATION"/debootstrap
 if [ ! -f "$BUILDLOCATION"/DontRestartPhase1"$BUILDARCH" ]
 then
   echo "Setting up chroot for downloading archives and software..."
-  "$BUILDLOCATION"/debootstrap/debootstrap --arch "$BUILDARCH" vivid "$BUILDLOCATION"/build/"$BUILDARCH"/phase_1 http://ubuntu.osuosl.org/ubuntu/
+  "$BUILDLOCATION"/debootstrap/debootstrap --arch "$BUILDARCH" vivid "$BUILDLOCATION"/build/"$BUILDARCH"/phase_1 http://archive.ubuntu.com/
   touch "$BUILDLOCATION"/DontRestartPhase1"$BUILDARCH"
 fi
 
@@ -66,6 +66,6 @@ if [ ! -f "$BUILDLOCATION"/DontRestartPhase2"$BUILDARCH" ]
 then
   #setup a really basic Ubuntu installation for the live cd
   echo "Setting up chroot for the Live CD..."
-  "$BUILDLOCATION"/debootstrap/debootstrap --arch "$BUILDARCH" vivid "$BUILDLOCATION"/build/"$BUILDARCH"/phase_2 http://ubuntu.osuosl.org/ubuntu/
+  "$BUILDLOCATION"/debootstrap/debootstrap --arch "$BUILDARCH" vivid "$BUILDLOCATION"/build/"$BUILDARCH"/phase_2 http://archive.ubuntu.com/
   touch "$BUILDLOCATION"/DontRestartPhase2"$BUILDARCH"
 fi
