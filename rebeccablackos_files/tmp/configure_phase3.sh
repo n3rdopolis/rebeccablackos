2b6e1500a3c04c1d578dea736dbb2b545ea02995
nerdopolis <nerdopolis@b52b6941-3400-464c-9f42-43200397181c>
2015-08-08 23:57:50 +0000

    Set the default user as a member of systemd-journal
    git-svn-id: https://svn.code.sf.net/p/rebeccablackos/code@3783 b52b6941-3400-464c-9f42-43200397181c

--- rebeccablackos_files/tmp/configure_phase3.sh
+++ rebeccablackos_files/tmp/configure_phase3.sh
@@ -60,7 +60,7 @@ mkdir debian
 touch debian/control
 #remove any old deb files for this package
 rm "/srcbuild/buildoutput/"rbos-rbos_*.deb
-checkinstall -y -D --nodoc --dpkgflags=--force-overwrite --install=yes --backup=no --pkgname=rbos-rbos --pkgversion=1 --pkgrelease=$(date +%s)  --maintainer=rbos@rbos --pkgsource=rbos --pkggroup=rbos --requires="expect,whois,dlocate,zenity,xterm,vpx-tools,screen" /tmp/configure_phase3_helper.sh
+checkinstall -y -D --nodoc --dpkgflags=--force-overwrite --install=yes --backup=no --pkgname=rbos-rbos --pkgversion=1 --pkgrelease=$(date +%s)  --maintainer=rbos@rbos --pkgsource=rbos --pkggroup=rbos --requires="expect,whois,dlocate,zenity,xterm,vpx-tools,screen,kbd" /tmp/configure_phase3_helper.sh
 cd $OLDPWD
 
 #Create a virtual configuration package for the waylandloginmanager
