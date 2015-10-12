4d23c242dab116064eb97f694a2a28ab3302b155
nerdopolis <nerdopolis@b52b6941-3400-464c-9f42-43200397181c>
2015-10-12 03:13:00 +0000

    Install acl for build_core
    git-svn-id: https://svn.code.sf.net/p/rebeccablackos/code@3918 b52b6941-3400-464c-9f42-43200397181c

--- rebeccablackos_files/tmp/configure_phase3.sh
+++ rebeccablackos_files/tmp/configure_phase3.sh
@@ -66,7 +66,7 @@ mkdir debian
 touch debian/control
 #remove any old deb files for this package
 rm "/srcbuild/buildoutput/"rbos-rbos_*.deb
-checkinstall -y -D --fstrans=no --nodoc --dpkgflags=--force-overwrite --install=yes --backup=no --pkgname=rbos-rbos --pkgversion=1 --pkgrelease=$(date +%s)  --maintainer=rbos@rbos --pkgsource=rbos --pkggroup=rbos --requires="expect,whois,dlocate,zenity,xterm,vpx-tools,screen,kbd" /tmp/configure_phase3_helper.sh
+checkinstall -y -D --fstrans=no --nodoc --dpkgflags=--force-overwrite --install=yes --backup=no --pkgname=rbos-rbos --pkgversion=1 --pkgrelease=$(date +%s)  --maintainer=rbos@rbos --pkgsource=rbos --pkggroup=rbos --requires="expect,whois,dlocate,zenity,xterm,vpx-tools,screen,kbd,checkinstall,acl" /tmp/configure_phase3_helper.sh
 cd $OLDPWD
 
 #Create a virtual configuration package for the waylandloginmanager
