ed4f93da3e70cffb0ba1b8aa5e841fbf94e7b7b2
nerdopolis <nerdopolis@b52b6941-3400-464c-9f42-43200397181c>
2016-07-04 14:57:40 +0000

    Clean up debootstrap dir if commanded to restart it
    git-svn-id: https://svn.code.sf.net/p/rebeccablackos/code@4236 b52b6941-3400-464c-9f42-43200397181c

--- rebeccablackos_builder.sh
+++ rebeccablackos_builder.sh
@@ -146,6 +146,7 @@ STARTTIME=$(date +%s)
 if [[ ! -e "$BUILDLOCATION"/debootstrap/debootstrap || ! -e "$BUILDLOCATION"/DontDownloadDebootstrapScript ]]
 then
   touch "$BUILDLOCATION"/DontDownloadDebootstrapScript
+  rm -rf "$BUILDLOCATION"/debootstrap
   mkdir -p "$BUILDLOCATION"/debootstrap
   FTPFILELIST=$(ftp -n -v ftp.debian.org << EOT
 ascii
