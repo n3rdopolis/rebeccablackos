1d6b50c465df43691ea6f6bbb74fbc0fc5b90e04
nerdopolis <nerdopolis@b52b6941-3400-464c-9f42-43200397181c>
2017-06-15 03:10:42 +0000

    test_RBOS_ISO.sh: start in a new dbus session
    git-svn-id: https://svn.code.sf.net/p/rebeccablackos/code@4836 b52b6941-3400-464c-9f42-43200397181c

--- externalbuilders/test_RBOS_ISO.sh
+++ externalbuilders/test_RBOS_ISO.sh
@@ -362,7 +362,7 @@ then
   NAMESPACE_ENTER mkdir -p "$MOUNTHOME"/liveisotest/unionmountpoint/var/run/dbus
   #give more information in the testuser .bashrc
   echo "
-(. /usr/bin/wlruntime_vars; /usr/bin/wlruntime_firstrun)
+(export \$(dbus-launch); . /usr/bin/wlruntime_vars; /usr/bin/wlruntime_firstrun)
 echo \"
 Weston Session commands:
 nested-defaultweston-caller
