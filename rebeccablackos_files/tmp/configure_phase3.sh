ca40c7e6d7864ff6bf41d59348f233cd8873acf3
nerdopolis <nerdopolis@b52b6941-3400-464c-9f42-43200397181c>
2020-07-13 23:39:02 +0000

    turn on the vTTYs
    git-svn-id: https://svn.code.sf.net/p/rebeccablackos/code@6469 b52b6941-3400-464c-9f42-43200397181c

--- rebeccablackos_files/tmp/configure_phase3.sh
+++ rebeccablackos_files/tmp/configure_phase3.sh
@@ -208,9 +208,9 @@ function PostInstallActions
   systemctl enable acpid.service
 
   #enable the virtual tty services.
-  #systemctl enable vtty-backend@.service
-  #systemctl enable vtty-frontend@.service
-  #ln -s /usr/lib/systemd/system/vtty-frontend@.service /etc/systemd/system/autovt@.service
+  systemctl enable vtty-backend@.service
+  systemctl enable vtty-frontend@.service
+  ln -s /usr/lib/systemd/system/vtty-frontend@.service /etc/systemd/system/autovt@.service
 
   #Add libraries under /opt to the ldconfig cache, for setcap'ed binaries
   echo /opt/lib >> /etc/ld.so.conf.d/aa_rbos_opt_libs.conf
